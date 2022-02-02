61c85e5d35c080732ab253af073b25c74ff7470b
lethycn <98864100+lethycn@users.noreply.github.com>
2022-02-01 21:48:12 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,12 +1,30 @@
-
+var cubo ;
 function setup() {
   createCanvas(400,400);
+  cubo=createSprite(200,200,30,30)
+
 }
 
 function draw() 
 {
   background(30);
+if(keyIsDown(RIGHT_ARROW)) {
+cubo.position.x+= 5;
+}
 
+if(keyIsDown(LEFT_ARROW)) {
+  cubo.position.x-= 5;
+  }
+
+ 
+  if(keyIsDown(UP_ARROW)) {
+    cubo.position.y-= 5;
+    }
+  
+    if(keyIsDown(DOWN_ARROW)) {
+      cubo.position.y+= 5;
+      }
+drawSprites();
 }
 
 
